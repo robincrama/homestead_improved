ed47823a82c5604fc1260f8fd01981a2d6cf3fa9
robincrama <robin.crama@thesecretlab.nl>
2016-02-17 16:55:59 +0100

    added react specific shell commands

--- after.sh
+++ after.sh
@@ -3,3 +3,7 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+sudo npm install -g browserify
+cd /home/vagrant/Code/Project/public
+npm install --no-bin-links
